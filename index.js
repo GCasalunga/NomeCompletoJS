451c5b4521dac81c98ffa2da912a2144ee716bce
Giovanna <giovannacasalunga@gmail.com>
2022-11-25 20:09:16 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,4 @@
+const Nome = prompt("Digite seu nome:");
+const Sobrenome = prompt("Digite seu sobrenome:");
+
+const NomeCompleto = confirm(`Seu nome completo é ${Nome} ${Sobrenome}\n Seu nome de catalogo é ${Sobrenome.toUpperCase()} ${Nome}`);
